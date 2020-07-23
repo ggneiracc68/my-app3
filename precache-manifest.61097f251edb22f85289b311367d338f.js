9aaaeeae5394e9f1533d0f4e99a847bd372e30f7
Gonzalo <ggneiracc68@gmail.com>
2020-07-23 05:48:15 -0500

    Updates

---
+++ precache-manifest.61097f251edb22f85289b311367d338f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b44765fa36da8ce464354dae7a4fe52e",
+    "url": "/my-app3/index.html"
+  },
+  {
+    "revision": "6a73db0716b5f16c304f",
+    "url": "/my-app3/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f97528078f08d8689be1",
+    "url": "/my-app3/static/js/2.2ebf5461.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-app3/static/js/2.2ebf5461.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a73db0716b5f16c304f",
+    "url": "/my-app3/static/js/main.9e110b12.chunk.js"
+  },
+  {
+    "revision": "2c060efd445f7167e499",
+    "url": "/my-app3/static/js/runtime-main.1f50ad4c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-app3/static/media/logo.5d5d9eef.svg"
+  }
+]);
